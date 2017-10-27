4dcaa1c014e8f1f3cea776242a43e786588032dd
wangfeiran <wangfeiran@qiyi.com>
2017-10-27 16:12:39 +0800

    modify a function

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
-alert(typeof undefined)
-var a ;
-alert(a.a)
+function getAttribute(obj,name,d) {
+	if(typeof obj == "object" && obj.hasOwnProperty(name)){
+		return obj[name];
+	}else{
+		return d ? d : "";
+	}
+}
+var a = undefined;
+console.log(getAttribute(a,"a",1))
